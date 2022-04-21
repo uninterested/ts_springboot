8b23e5a815e022a10dd4d03db94f88acaa28f3c5
uninterested <10614170+uninterested@user.noreply.gitee.com>
2022-04-21 21:56:50 +0800

    update

--- demo/module.ts
+++ demo/module.ts
@@ -1,5 +1,5 @@
 import { IOCDecorator } from "../core/decorators/ioc_decorator";
-import UserController from "./controller/user";
+import UserController from "./controller/user_controller";
 
 @IOCDecorator({
   controllers: [UserController]
