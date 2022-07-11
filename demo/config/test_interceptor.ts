b3619104b1e9aadc294e0502a5e86400ab149472
朱江华 <zhujianghua@wanmi.com>
2022-07-11 10:55:41 +0800

    update interpotor with async

--- demo/config/test_interceptor.ts
+++ demo/config/test_interceptor.ts
@@ -2,10 +2,21 @@ import { IncomingMessage, ServerResponse } from "http";
 import HandleInterceptor from "../../core/config/interceptor/handle_interceptor";
 
 export default class TestInterpotor implements HandleInterceptor {
-  preHandle(req: IncomingMessage, res: ServerResponse, handle: any) {
+  // 同步处理
+  preHandle(req: IncomingMessage, res: ServerResponse, handle: any): boolean {
     console.log('123123: ', '测试拦截器生效')
     return true
   }
+
+  // 异步处理
+  // preHandle(req: IncomingMessage, res: ServerResponse, handle: any): Promise<boolean> {
+  //   console.log('123123: ', '测试拦截器生效')
+  //   return new Promise(r => {
+  //     setTimeout(() => {
+  //       r(true)
+  //     }, 3000);
+  //   })
+  // }
   afterCompletion(req: IncomingMessage, res: ServerResponse, handle: any): void {
     console.log('123123', 'afterCompletion')
   }
